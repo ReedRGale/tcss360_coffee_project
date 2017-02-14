d63000c225a6e45f41967cdac1f790c804b8f3b4
Jared Lowery <jadersleet@gmail.com>
2017-02-14 13:06:01 -0800

    Changed relationships in web.xml

--- src/main/java/com/mycompany/tcss360_coffee_project/UserService.java
+++ src/main/java/com/mycompany/tcss360_coffee_project/UserService.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.sample_maven_web_app;
+package com.mycompany.tcss360_coffee_project;
 
 import data.Model;
 import javax.ws.rs.core.Context;
