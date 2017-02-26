156a75ff22ae02195befe4dac2ea10e0181da9cb
xutedin253 <thomasxu23@gmail.com>
2017-02-25 19:10:22 -0800

    akdjf

--- src/main/java/com/mycompany/tcss360_coffee_project/ShopService.java
+++ src/main/java/com/mycompany/tcss360_coffee_project/ShopService.java
@@ -110,10 +110,10 @@ public class ShopService
                 
 //                + "$(document).on('ready', myMap);\n"
                 
-                + "var markers = [\n"
-                + " [\"Starbucks\", Starbucks Seattle Washington],\n"
-                + " [\"Anthem\", 6244 25th Ave NE Seattle Washington],\n"
-                + " [\"Forza\", 6244 25th Ave NE Seattle Washington]\n"
+                + "var markers = ["
+//                + " [\"Starbucks\", Starbucks Seattle Washington],\n"
+                + " [\"Anthem\", 6244 25th Ave NE Seattle Washington],"
+                + " [\"Forza\", 6244 25th Ave NE Seattle Washington]"
                 + "];\n"
                 
                 //initializer of the map
