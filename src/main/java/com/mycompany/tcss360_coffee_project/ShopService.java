88b1390ec3273f1dcd752180875a1e5cc90ca81a
Garrett Wyler <grwyler@gmail.com>
2017-03-12 19:19:40 -0700

    changed json to application/json

--- src/main/java/com/mycompany/tcss360_coffee_project/ShopService.java
+++ src/main/java/com/mycompany/tcss360_coffee_project/ShopService.java
@@ -74,9 +74,9 @@ public class ShopService {
             sb.append(
               "function getshops(){" 
             + "var url='https://team2coffeefinder.herokuapp.com/api/shops';" 
-            + "$.ajax({type: 'GET',url: url,datatype: 'json',success: render});"
+            + "$.ajax({type: 'GET',url: url,datatype: 'application/json',success: render});"
             + "var url='https://gentle-coast-59786.herokuapp.com/tcss360/coffeeShop/api/shops';" 
-            + "$.ajax({type: 'GET',url: url,datatype: 'json',success: render});}"
+            + "$.ajax({type: 'GET',url: url,datatype: 'application/json',success: render});}"
             );
             
 
